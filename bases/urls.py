4ec05bbef1383690bb5b7df556e6c02725aae60f
raul eduardo Trejo Suarez <raultrejo@hotmail.com>
2021-12-02 14:56:04 -0600

    Se creo el Login y es funcional

--- bases/urls.py
+++ bases/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
+from django.contrib.auth import views as auth_views
 from bases.views import *
+
 
 app_name = "config"
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='bases/login.html'),
+         name='login/'),
+    path('logout/',
+         auth_views.LogoutView.as_view(template_name='bases/login.html'),
+         name='logout/'),
 ]
